f2dfc05d3cbed44da499cac5d35d8e643b46392d
Khoi Nguyen <khoifish@gmail.com>
2019-01-16 07:27:51 +0700

    Final image from book 2, next weekend

--- Source/main.cpp
+++ Source/main.cpp
@@ -96,7 +96,7 @@ static IHitable* createTwoPerlinSpheres()
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
-static IHitable* SimpleLightScene()
+static IHitable* simpleLightScene()
 {
 	Texture* perlinTex = new NoiseTexture(4);
 	IHitable** list = new IHitable*[4];
@@ -110,7 +110,7 @@ static IHitable* SimpleLightScene()
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
-static IHitable* CornellBox(bool smoke)
+static IHitable* cornellBox(bool smoke)
 {
 	IHitable** list = new IHitable*[8];
 	int i = 0;
@@ -160,12 +160,100 @@ static IHitable* CornellBox(bool smoke)
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
+static IHitable* finalScene()
+{
+	const int numBoxes = 20;
+	int total = 0;
+
+	IHitable** list     = new IHitable*[30];
+	IHitable** boxlist  = new IHitable*[10000];
+	IHitable** boxlist2 = new IHitable*[10000];
+	Material*  white    = new MLambertian(new ConstantTexture(Vec3(0.73f, 0.73f, 0.73f)));
+	Material*  ground   = new MLambertian(new ConstantTexture(Vec3(0.48f, 0.83f, 0.53f)));
+
+	// Create random hitable boxes, in BVH tree
+	{
+		int b = 0;
+		for (int i = 0; i < numBoxes; i++)
+		{
+			for (int j = 0; j < numBoxes; j++)
+			{
+				float w = 100;
+				float x0 = -1000 + i * w;
+				float z0 = -1000 + j * w;
+				float y0 = 0;
+				float x1 = x0 + w;
+				float y1 = 100 * (RandomFloat() + 0.01f);
+				float z1 = z0 + w;
+
+				boxlist[b++] = new HitableBox(Vec3(x0, y0, z0), Vec3(x1, y1, z1), ground);
+			}
+		}
+		list[total++] = new BVHNode(boxlist, b, 0, 1);
+	}
+
+	// Create light
+	{
+		Material *light = new MDiffuseLight(new ConstantTexture(Vec3(7, 7, 7)));
+		list[total++] = new XYZRect(XYZRect::XZ, 123, 423, 147, 412, 554, light);
+	}
+
+	// Moving sphere
+	{
+		Vec3 center(400, 400, 200);
+		list[total++] = new MovingSphere(center, center + Vec3(30, 0, 0), 0, 1, 50, new MLambertian(new ConstantTexture(Vec3(0.7f, 0.3f, 0.1f))));
+	}
+
+	// Dielectric and metal spheres
+	{
+		list[total++] = new Sphere(Vec3(260, 150, 45), 50, new MDielectric(1.5f));
+		list[total++] = new Sphere(Vec3(0, 150, 145), 50, new MMetal(Vec3(0.8f, 0.8f, 0.9f), 10.0f));
+	}
+
+	// Volumes
+	{
+		IHitable *boundary = new Sphere(Vec3(360, 150, 145), 70, new MDielectric(1.5f));
+		list[total++] = boundary;
+		list[total++] = new ConstantMedium(boundary, 0.2f, new ConstantTexture(Vec3(0.2f, 0.4f, 0.9f)));
+
+
+		boundary = new Sphere(Vec3(0, 0, 0), 5000, new MDielectric(1.5f));
+		list[total++] = new ConstantMedium(boundary, 0.0001f, new ConstantTexture(Vec3(1.0f, 1.0f, 1.0f)));
+	}
+
+	// Image texture sphere
+	{
+		int nx, ny, nn;
+		unsigned char *texData = stbi_load("guitar.jpg", &nx, &ny, &nn, 0);
+		Material *emat = new MLambertian(new ImageTexture(texData, nx, ny));
+		list[total++] = new Sphere(Vec3(400, 200, 400), 100, emat);
+	}
+
+	// Perlin noise sphere
+	{
+		Texture *perTex = new NoiseTexture(0.1f);
+		list[total++] = new Sphere(Vec3(220, 280, 300), 80, new MLambertian(perTex));
+	}
+
+	// Translated, rotated spheres in BVH tree
+	{
+		int ns = 1000;
+		for (int j = 0; j < ns; j++)
+		{
+			boxlist2[j] = new Sphere(Vec3(165 * RandomFloat(), 165 * RandomFloat(), 165 * RandomFloat()), 10, white);
+		}
+		list[total++] = new HitableTranslate(new HitableRotateY(new BVHNode(boxlist2, ns, 0.0, 1.0), 15), Vec3(-100, 270, 395));
+	}
+
+	return new HitableList(list, total);
+}
+
 int main()
 {
 	// Raytracer params
 	const int    outputWidth  = 1000;
 	const int    outputHeight = 512;
-	const int    numSamples   = 200;
+	const int    numSamples   = 5000;
 	const int    maxDepth     = 50;
 	const int    numThreads   = 8;
 
@@ -196,6 +284,7 @@ int main()
 	}
 
 	// Cornell box
+	if (false)
 	{
 		// Camera options
 		const Vec3   lookFrom = Vec3(278, 278, -800);
@@ -213,11 +302,33 @@ int main()
 			shutterTime0, shutterTime1);
 
 		// Render and write out image
-		tracer.Render(cam, CornellBox(false));
+		tracer.Render(cam, cornellBox(false));
 		tracer.WriteOutputToPPMFile(std::ofstream("cornell.ppm"));
 
-		tracer.Render(cam, CornellBox(true));
+		tracer.Render(cam, cornellBox(true));
 		tracer.WriteOutputToPPMFile(std::ofstream("cornell_smoke.ppm"));
+	}
+
+	// Final
+	{
+		// Camera options
+		const Vec3   lookFrom = Vec3(278, 278, -800);
+		const Vec3   lookAt = Vec3(278, 278, 0);
+		const Vec3   upVec = Vec3(0, 1, 0);
+		const float  vertFov = 40.f;
+		const float  aspect = float(outputWidth) / float(outputHeight);
+		const float  aperture = 0.0f;
+		const float  distToFocus = 10.f;
+		const float  shutterTime0 = 0.f;
+		const float  shutterTime1 = 1.f;
+		Camera cam(
+			lookFrom, lookAt, upVec,
+			vertFov, aspect, aperture, distToFocus,
+			shutterTime0, shutterTime1);
+
+		// Render and write out image
+		tracer.Render(cam, finalScene());
+		tracer.WriteOutputToPPMFile(std::ofstream("final.ppm"));
 	}
 
 	// Done

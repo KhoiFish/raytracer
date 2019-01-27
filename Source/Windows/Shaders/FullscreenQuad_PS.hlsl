6ad054663d0d178b843435fc8dfd71ef0dfa28a1
Khoi Nguyen <khoifish@gmail.com>
2019-01-27 15:18:00 +0700

    Refactor fullscreen quad shader code

--- Source/Windows/Shaders/FullscreenQuad_PS.hlsl
+++ Source/Windows/Shaders/FullscreenQuad_PS.hlsl
@@ -1,24 +1,17 @@
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
 struct PixelShaderInput
 {
-    float4 PositionVS : POSITION;
-    float3 NormalVS   : NORMAL;
-    float2 TexCoord   : TEXCOORD;
+    float2 TexCoord : TEXCOORD;
 };
 
-struct Material
-{
-    float4   Emissive;
-    float4   Ambient;
-    float4   Diffuse;
-    float4   Specular;
-    float    SpecularPower;
-    float3   Padding;
-};
+// ----------------------------------------------------------------------------------------------------------------------------
 
-Texture2D                  DiffuseTexture         : register(t0);
-SamplerState               LinearRepeatSampler    : register(s0);
-ConstantBuffer<Material>   MaterialCB             : register(b0, space1);
+Texture2D     DiffuseTexture         : register(t0);
+SamplerState  LinearRepeatSampler    : register(s0);
 
+// ----------------------------------------------------------------------------------------------------------------------------
 
 float4 main( PixelShaderInput IN ) : SV_Target
 {
--- Source/Windows/Shaders/FullscreenQuad_VS.hlsl
+++ Source/Windows/Shaders/FullscreenQuad_VS.hlsl
@@ -1,12 +1,5 @@
-struct Mat
-{
-    matrix ModelMatrix;
-    matrix ModelViewMatrix;
-    matrix InverseTransposeModelViewMatrix;
-    matrix ModelViewProjectionMatrix;
-};
 
-ConstantBuffer<Mat> MatCB : register(b0);
+// ----------------------------------------------------------------------------------------------------------------------------
 
 struct VertexPositionNormalTexture
 {
@@ -16,13 +9,14 @@ struct VertexPositionNormalTexture
     uint   Id       : SV_VERTEXID;
 };
 
+// ----------------------------------------------------------------------------------------------------------------------------
 struct VertexShaderOutput
 {
-    float4 PositionVS : POSITION;
-    float3 NormalVS   : NORMAL;
-    float2 TexCoord   : TEXCOORD;
-    float4 Position   : SV_Position;
+    float2 TexCoord : TEXCOORD;
+    float4 Position : SV_Position;  // Note the position needs to be last, I don't know why??
 };
+
+// ----------------------------------------------------------------------------------------------------------------------------
 
 VertexShaderOutput main(VertexPositionNormalTexture IN)
 {
@@ -30,16 +24,13 @@ VertexShaderOutput main(VertexPositionNormalTexture IN)
 
     uint id = IN.Id;
 
-    OUT.PositionVS.x = (float)(id / 2) * 4.0f - 1.0f;
-    OUT.PositionVS.y = (float)(id % 2) * 4.0f - 1.0f;
-    OUT.PositionVS.z = 0.0f;
-    OUT.PositionVS.w = 1.0f;
+    OUT.Position.x = (float)(id / 2) * 4.0f - 1.0f;
+    OUT.Position.y = (float)(id % 2) * 4.0f - 1.0f;
+    OUT.Position.z = 0.0f;
+    OUT.Position.w = 1.0f;
 
     OUT.TexCoord.x = (float)(id / 2) * 2.0f;
     OUT.TexCoord.y = 1.0f - (float)(id % 2) * 2.0f;
-
-    OUT.Position = OUT.PositionVS;
-    OUT.NormalVS = mul((float3x3)MatCB.InverseTransposeModelViewMatrix, IN.Normal);
 
     return OUT;
 }

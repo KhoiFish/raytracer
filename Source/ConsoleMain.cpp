c73f1010242b6b6d16a9ca1d25da1842a7f4ba95
Khoi Nguyen <khoifish@gmail.com>
2019-01-31 06:59:17 +0700

    Add commandline parsing

--- Source/ConsoleMain.cpp
+++ Source/ConsoleMain.cpp
@@ -4,12 +4,12 @@
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
-static int    sOutputWidth      = 256;
-static int    sOutputHeight     = 256;
+static int    sOutputWidth      = 1024;
+static int    sOutputHeight     = 1024;
 static float  sAspect           = float(sOutputWidth) / float(sOutputHeight);
-static int    sNumSamplesPerRay = 1;
+static int    sNumSamplesPerRay = 5000;
 static int    sMaxScatterDepth  = 50;
-static int    sNumThreads       = 6;
+static int    sNumThreads       = 16;
 
 static bool   sSceneEnabled[MaxScene] =
 {
@@ -40,9 +40,55 @@ static void raytraceAndPrintProgress(Raytracer& tracer, Camera& cam, WorldScene*
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
-int main()
+static void parseCommandline(int argc, const char* argv[])
+{
+    for (int i = 0; i < argc; i++)
+    {
+        if (strstr(argv[i], "width") != nullptr && (i + 1) < argc)
+        {
+            sOutputWidth = atoi(argv[++i]);
+        }
+        else if (strstr(argv[i], "height") != nullptr && (i + 1) < argc)
+        {
+            sOutputHeight = atoi(argv[++i]);
+        }
+        else if (strstr(argv[i], "samples") != nullptr && (i + 1) < argc)
+        {
+            sNumSamplesPerRay = atoi(argv[++i]);
+        }
+        else if (strstr(argv[i], "depth") != nullptr && (i + 1) < argc)
+        {
+            sMaxScatterDepth = atoi(argv[++i]);
+        }
+        else if (strstr(argv[i], "threads") != nullptr && (i + 1) < argc)
+        {
+            sNumThreads = atoi(argv[++i]);
+        }
+        else if (strstr(argv[i], "disableScene") != nullptr && (i + 1) < argc)
+        {
+            const int sceneNum = atoi(argv[++i]);
+            if (sceneNum < MaxScene)
+            {
+                sSceneEnabled[sceneNum] = false;
+            }
+        }
+    }
+
+    if (argc <= 1)
+    {
+        printf("Commandline usage:\n\twidth [num]  height [num]  samples [num]  depth [num]  threads [num]  disableScene [sceneNum]\n");
+    }
+
+    printf("Current tracing parameters:\n\tresolution:%dx%d numSamples:%d scatterDepth:%d numThreads:%d\n",
+        sOutputWidth, sOutputHeight, sNumSamplesPerRay, sMaxScatterDepth, sNumThreads);
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
+int main(int argc, const char* argv[])
 {
     // Create ray tracer
+    parseCommandline(argc, argv);
     Raytracer tracer(sOutputWidth, sOutputHeight, sNumSamplesPerRay, sMaxScatterDepth, sNumThreads);
 
     // Random scene
